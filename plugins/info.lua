7f3151639274a53b8152d9b7a0493698267b4d32
mehdi <mehdizare863@yahoo.com>
2016-03-05 23:56:40 +0330

    Create info.lua

---
+++ plugins/info.lua
@@ -0,0 +1,97 @@
+[en]
+IR_GOOD = [Info rank] Congratulations, you have advanced %i place(s) !!
+IR_BAD = [Info rank] Noob, you have gone down %i place(s) !!
+IR_RANK = [Info rank] Your rank : %i of %i
+IR_ENABLE = [Info rank] You have enabled messages
+IR_DISABLE = [Info rank] You have disabled messages
+
+[fr]
+IR_GOOD = [Info rank] Bravo, tu as gagné %i place(s) !!
+IR_BAD = [Info rank] Noob, tu as perdu %i place(s) !!
+IR_RANK = [Info rank] Ton rang : %i sur %i joueurs
+IR_ENABLE = [Info rank] Messages actifs
+IR_DISABLE = [Info rank] Messages inactifs
+
+[cz]
+IR_GOOD = [Info rank] Gratulujeme, stoupas v ranku o %i mist !!
+IR_BAD = [Info rank] Noobe, padas v ranku o %i mist !!
+IR_RANK = [Info rank] Tvuj rank je : %i z %i
+IR_ENABLE = [Info rank] Povolil jsi Rank info
+IR_DISABLE = [Info rank] Zablokoval jsi Rank info 
+
+[ro]
+IR_GOOD = [Info rank] Felicitari, ai avansat %i loc(uri) !!
+IR_BAD = [Info rank] Noob, ai pierdut %i loc(uri) !!
+IR_RANK = [Info rank] Pozitia ta in top : %i din %i
+IR_ENABLE = [Info rank] Ai avtivat mesajele
+IR_DISABLE = [Info rank] Ai dezactivat mesajele
+
+[nl]
+IR_GOOD = [Info rank] Gefeliciteerd, je bent %i plaats(en) vooruit gegaan!!
+IR_BAD = [Info rank] Noob, je bent %i plaats(en) achteruit gegaan!!
+IR_RANK = [Info rank] Je rank : %i van %i
+IR_ENABLE = [Info rank] Je hebt berichten ingeschakeld
+IR_DISABLE = [Info rank] Je hebt berichten uitgeschakeld
+
+[lt]
+IR_GOOD = [Info rank] Sveikiname, tu pakilai aukstyn per %i vieta(s) !!
+IR_BAD = [Info rank] Mulki, tu nukritai zemyn per %i vieta(s) !!
+IR_RANK = [Info rank] Tavo statistika : %i is %i
+IR_ENABLE = [Info rank] Tu ijungei zinutes
+IR_DISABLE = [Info rank] Tu isjungei zinutes
+
+[es]
+IR_GOOD = [Info rank] Felicidad, Avanzaste %i lugar(es) !!
+IR_BAD = [Info rank] Principiante, Perdiste %i lugar(es) !!
+IR_RANK = [Info rank] Tu rango : %i de %i
+IR_ENABLE = [Info rank] Habilitaste los mensajes de rango
+IR_DISABLE = [Info rank] Deshabilitaste los mensajes de rango
+
+[de]
+IR_GOOD = [Info rank] Glueckwunsch, du bist um %i Pla(e)tz(e) vorgerueckt!!
+IR_BAD = [Info rank] Anfaenger, du bist um %i Pla(e)tz(e) gefallen!!
+IR_RANK = [Info rank] Dein Rang : %i von %i
+IR_ENABLE = [Info rank] Du hast die Nachrichten eingeschaltet.
+IR_DISABLE = [Info rank] Du hast die Nachrichten abgeschaltet.
+
+[bp]
+IR_GOOD = [Info rank] Parabens, subiste %i posicao(oes) !!
+IR_BAD = [Info rank] Noob, desceste %i posicao(oes) !!
+IR_RANK = [Info rank] Teu rank : %i de %i
+IR_ENABLE = [Info rank] Activaste as mensagens
+IR_DISABLE = [Info rank] Desactivaste as mensagens
+
+[lv]
+IR_GOOD = [Info ranks] Apsveicu, Tu esi uz priekšu pavirzijies pa %i vietu(am) !!
+IR_BAD = [Info ranks] Nubs, Tu esi nokrities pa %i vietu(am) !!
+IR_RANK = [Info ranks] Tavs ranks : %i no %i
+IR_ENABLE = [Info ranks] Tu esi iestatijis zinas.
+IR_DISABLE = [Info ranks] Tu esi atsledzis zinas.
+
+[pl]
+IR_GOOD = [Rank info] Gratulacje, awansowales o %i miejsce !!
+IR_BAD = [Rank info] Noobku, spadles w ranku o %i miejsc !!
+IR_RANK = [Rank infok] Twoj ranking wynosi : %i of %i
+IR_ENABLE = [Rank info] Odblokowales Rank info
+IR_DISABLE = [Rank infok] Zablokowales Rank info
+
+[hu]
+IR_GOOD = [Info rank] Gratulálunk! Felkerültél a %i. helyre !!
+IR_BAD = [Info rank] Noob, leestél a %i. helyre !!
+IR_RANK = [Info rank] A rangod : %i a %i-ból
+IR_ENABLE = [Info rank] Engedélyezted az üzeneteket.
+IR_DISABLE = [Info rank] Letiltottad az üzeneteket.
+
+[sr]
+IR_GOOD = [Info rank] Cestitamo, napredovali ste za %i mesta !!
+IR_BAD = [Info rank] Pali ste na ranku za %i mesta !!
+IR_RANK = [Info rank] Tvoj rank : %i od %i
+IR_ENABLE = [Info rank] Imas ukljucene poruke
+IR_DISABLE = [Info rank] Imas iskljucene poruke
+
+[tr]
+IR_GOOD = [Info rank] Tebrikler, %i yere ilerledin !!
+IR_BAD = [Info rank] Yere dustun ahbap %i !!
+IR_RANK = [Info rank] Dereceniz : %i / %i
+IR_ENABLE = [Info rank] Mesajlarinizi etkinlestirdiniz
+IR_DISABLE = [Info rank] Mesajlarinizi kapattiniz
